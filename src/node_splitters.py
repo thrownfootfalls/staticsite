143544f937362416e242219a278327bbc714e72e
thrownfootfalls <alastairrgmurray@gmail.com>
2025-02-10 22:34:37 +0000

    Renamed files before the name becomes misleading

--- src/node_splitters.py
+++ src/node_splitters.py
@@ -4,12 +4,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
         texts = old_node.text.split(delimiter)
-        print(old_node.text)
-        print(texts)
         for i, text in enumerate(texts):
-#            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
             current_type = [TextType.NORMAL, text_type][i%2] # aiternate "on" and "off"
-            #print(current_type)
             new_nodes.append(TextNode(text, current_type))
     return new_nodes
 
